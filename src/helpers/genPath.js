621f0c6e86bfdec4e8f24772384a4c7240f3d84e
Dmytro Barylo <bdpochta@gmail.com>
2019-01-20 14:32:15 +0200

    Simplify genPath method

--- src/helpers/genPath.js
+++ src/helpers/genPath.js
@@ -1,4 +1,5 @@
-export default (points, smooth) => {
+export default (pnts, smooth) => {
+  const points = [...pnts];
   const start = points.shift();
 
   return (
@@ -6,22 +7,12 @@ export default (points, smooth) => {
     points.map((point, index) => {
       if (!smooth) return ` L${point.x},${point.y}`;
 
-      const next = points[index + 1];
       const prev = points[index - 1] || start;
-      const distance = (points[0].x - start.x) / 2;
-      if (index == 0) {
-        return ` C ${prev.x},${prev.y} ${distance + prev.x},${point.y} ${
-          point.x
-        },${point.y}`;
-      } else if (next) {
-        return ` C ${distance + prev.x},${prev.y} ${distance + prev.x},${
-          point.y
-        } ${point.x},${point.y}`;
-      } else {
-        return ` C ${distance + prev.x},${prev.y} ${point.x},${point.y} ${
-          point.x
-        },${point.y}`;
-      }
+      const distance = points[0].x - start.x;
+      const bezierX = distance / 2;
+      return ` C ${bezierX + prev.x},${prev.y} ${bezierX + prev.x},${point.y} ${
+        point.x
+      },${point.y}`;
     })
   );
 };
