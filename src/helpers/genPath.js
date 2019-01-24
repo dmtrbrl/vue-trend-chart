539a6406bd9a8e0246b253f1bd6863e48192caf5
Dmytro Barylo <bdpochta@gmail.com>
2019-01-24 11:41:24 +0200

    Refactor genPath helper

--- src/helpers/genPath.js
+++ src/helpers/genPath.js
@@ -2,6 +2,8 @@ export default (pnts, smooth, { maxY }, strokeWidth) => {
   const points = [...pnts];
   const start = points.shift();
   const end = points[points.length - 1];
+  const distance = points[0].x - start.x;
+  const bezierX = distance / 2;
 
   // Create Line Path
   let linePath =
@@ -10,12 +12,10 @@ export default (pnts, smooth, { maxY }, strokeWidth) => {
       if (!smooth) return ` L${point.x},${point.y}`;
 
       const prev = points[index - 1] || start;
-      const distance = points[0].x - start.x;
-      const bezierX = distance / 2;
 
-      return ` C ${bezierX + prev.x},${prev.y} ${bezierX + prev.x},${point.y} ${
-        point.x
-      },${point.y}`;
+      return ` C ${bezierX + prev.x},${prev.y} 
+                 ${bezierX + prev.x},${point.y} 
+                 ${point.x},${point.y}`;
     });
 
   // Create Fill Path
