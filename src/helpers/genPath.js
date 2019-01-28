080209d1f6ba07e7bcce1943ea333d7bd4a726ed
Dmytro Barylo <bdpochta@gmail.com>
2019-01-28 13:54:07 +0200

    Fix linePath generation

--- src/helpers/genPath.js
+++ src/helpers/genPath.js
@@ -6,17 +6,16 @@ export default (pnts, smooth, { maxY }) => {
   const bezierX = distance / 2;
 
   // Create Line Path
-  let linePath =
-    `M ${start.x},${start.y}` +
-    points.map((point, index) => {
-      if (!smooth) return ` L${point.x},${point.y}`;
-
+  let linePath = `M ${start.x},${start.y}`;
+  points.forEach((point, index) => {
+    if (!smooth) linePath += ` L${point.x},${point.y}`;
+    else {
       const prev = points[index - 1] || start;
-
-      return ` C ${bezierX + prev.x},${prev.y} ${bezierX + prev.x},${point.y} ${
-        point.x
-      },${point.y}`;
-    });
+      linePath += ` C ${bezierX + prev.x},${prev.y} ${bezierX + prev.x},${
+        point.y
+      } ${point.x},${point.y}`;
+    }
+  });
 
   // Create Fill Path
   let fillPath = linePath;
