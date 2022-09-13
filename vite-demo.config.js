4a53bc17fca7d8bcf2556f62a227a178de70fa91
Dmytro Barylo <bdpochta@gmail.com>
2022-09-13 12:31:06 +0200

    Fix demo config

--- vite-demo.config.js
+++ vite-demo.config.js
@@ -4,6 +4,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "",
   plugins: [vue()],
   root: resolve(__dirname, "demo"),
   build: {
